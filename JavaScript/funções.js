f405a4af62dcf4771960c35f115d2dbaf6dfc0a2
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2022-10-23 15:50:10 -0300

    Add files via upload

---
+++ JavaScript/funções.js
@@ -0,0 +1,10 @@
+function Function(){}
+
+    function somacomum (a,b){
+    return(a+b)}
+    
+    arrow = (a,b)=>{return a**b}
+
+console.log(somacomum(5,8))
+console.log(arrow(5,5))
+console.log(Function())
