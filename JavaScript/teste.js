d1dddffb93c57b55c4bcb70d55c67e79b423c24d
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2022-10-22 21:36:05 -0300

    Delete teste.js

--- JavaScript/teste.js
+++
@@ -1,9 +0,0 @@
-function imc(){
-    var altura = parseFloat(document.getElementById("altura").value);
-    var peso = parseFloat(document.getElementById("peso").value);
-    var imc = peso/ (altura*2) ;
-    if(imc >= 18 && imc <= 25 )
-    if(imc==20)
-    alert("Parabens você está com o peso ideal");
-    if(imc < 18)
-    ("Cuidado! você precisa aumentar seu peso")}
