1dd722f0c6c5d740196a8aa0bf4f46c99c5aa8e8
Jorge Yohan <116468195+iNahoy@users.noreply.github.com>
2022-11-02 18:34:00 -0300

    Create html.js

---
+++ JavaScript/BD-JS/html.js
@@ -0,0 +1,5 @@
+const express = require ('express');
+const app = express();
+
+app.get("/",async (req,res,) => {res.send("Teste de rotas");});
+app.listen(2100, () => { console.log("Servidor funcionando")});
